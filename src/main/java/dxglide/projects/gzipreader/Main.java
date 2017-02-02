a3418ccad560ae7f76c9c756cda66b269c63f9f1
dxglide <dxglide@gmail.com>
2017-02-02 20:38:18 +0200

    changes

---
+++ src/main/java/dxglide/projects/gzipreader/Main.java
@@ -0,0 +1,10 @@
+package dxglide.projects.gzipreader;
+
+/**
+ * Created by dxglide on 17.2.2.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
